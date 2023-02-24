cbe977200104958c880213a65d3c933ba44bdc95
Agustin PLuto <agustinpluto@gmail.com>
2023-02-24 03:43:44 -0300

    asd

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -2,28 +2,29 @@
 
 class conexion {
 
-        private $server; 
+        private $server;
         private $user;
-        private $password; 
+        private $password;
         private $database;
         private $port;
         private $conexion;
-
+    
+    
         function __construct(){
             $listadatos = $this->datosConexion();
             foreach ($listadatos as $key => $value) {
-                $this->server = $value.['server'];
-                $this->user = $value.['user'];
-                $this->password = $value.['password'];
-                $this->database = $value.['database'];
-                $this->port = $value.['port'];
+                $this->server = $value['server'];
+                $this->user = $value['user'];
+                $this->password = $value['password'];
+                $this->database = $value['database'];
+                $this->port = $value['port'];
             }
-
             $this->conexion = new mysqli($this->server,$this->user,$this->password,$this->database,$this->port);
-            if ($this->conexion->connect_errno){
-                echo "No se pudo conectar"
+            if($this->conexion->connect_errno){
+                echo "algo va mal con la conexion";
+                die();
             }
-
+    
         }
 
         private function datosConexion() {
