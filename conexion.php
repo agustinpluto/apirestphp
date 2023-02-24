044c8a3a36b3c8cd3293b4fb6a64f225099b45b8
Agustin PLuto <agustinpluto@gmail.com>
2023-02-24 04:58:14 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -1,3 +1,22 @@
 <?php
-phpinfo();
+
+    function establecer_conexion() {
+        $conn = mysqli_connect("127.0.0.1", "agustin", "Ireliagod1!", "apirest");
+        echo "Conexión establecida";
+      }
+      
+      $sql = "SELECT * FROM usuarios";
+      $resultado = mysqli_query(establecer_conexion(), $consulta);
+      $registros = array();
+
+      while ($fila = mysqli_fetch_assoc($resultado)) {
+        $registros[] = $fila;
+      }
+
+      $json = json_encode($registros);
+      echo $json;
+      mysqli_close($conexion);
+
+      
+
 ?>
