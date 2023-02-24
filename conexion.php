37f2d39a9146c47a16093c9cc6a526c67a9acdf7
Agustin PLuto <agustinpluto@gmail.com>
2023-02-24 04:39:15 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -5,6 +5,15 @@
         echo "Conexión establecida";
       }
       
+      $consulta = mysqli_prepare($conexion, "SELECT * FROM usuarios");
+      mysqli_stmt_bind_param($consulta, "s", $valor_a_buscar);
+      mysqli_stmt_execute($consulta);
+      $resultado = mysqli_stmt_get_result($consulta);
+      while ($fila = mysqli_fetch_assoc($resultado)) {
+        echo $fila["nombre_de_campo"];
+      }
+      mysqli_close($conexion);
+      
       establecer_conexion();
 
 ?>
